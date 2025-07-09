eb00be2336aa9495caf40df0849e5f30192c3291
yamabouz <52478552+yamabouz@users.noreply.github.com>
2025-07-09 16:48:39 +0900

    新規作成

---
+++ src/main/java/jp/spring/boatrace/BoatraceApplication.java
@@ -0,0 +1,13 @@
+package jp.spring.boatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatraceApplication.class, args);
+	}
+
+}
